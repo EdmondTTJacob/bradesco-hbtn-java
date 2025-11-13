f7601f3912e99c683ae34455847a47f39ac08650
EdmondTTJacob <edmondjacob.amd@gmail.com>
2025-11-13 14:39:53 +0000

    feat: heranca/empregados

---
+++ Heranca/empregados/Empregado.java
@@ -0,0 +1,22 @@
+public class Empregado {
+    private double salarioFixo;
+
+    public Empregado(double salarioFixo) {
+        this.salarioFixo = salarioFixo;
+    }
+
+    public double getSalarioFixo() {
+        return this.salarioFixo;
+    }
+
+    public double calcularBonus(Departamento departamento) {
+        if (departamento.alcancouMeta()) {
+            return this.salarioFixo * 0.10;
+        }
+        return 0;
+    }
+
+    public double calcularSalarioTotal(Departamento departamento) {
+        return this.salarioFixo + this.calcularBonus(departamento);
+    }
+}
