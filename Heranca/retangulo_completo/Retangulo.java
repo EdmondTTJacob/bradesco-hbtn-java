bfb0b6f36f4cccf3e5f2c22bd37462d14da32f68
EdmondTTJacob <edmondjacob.amd@gmail.com>
2025-11-13 15:10:00 +0000

    heranca/retanguloCompleto

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,36 @@
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return this.largura;
+    }
+
+    public void setLargura(double valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = valor;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(double valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = valor;
+    }
+
+    @Override
+    public double area() {
+        return this.largura * this.altura;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Retangulo] %.2f / %.2f", this.largura, this.altura);
+    }
+}
