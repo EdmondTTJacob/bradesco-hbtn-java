38a167300200decce6ea1a4c97548264f606c359
EdmondTTJacob <edmondjacob.amd@gmail.com>
2025-11-13 13:57:12 +0000

    personagem consistencia

--- Classes_Objetos/personagem/Program.java
+++ Classes_Objetos/personagem/Program.java
@@ -1,15 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 public class Program {
-    public static void main(String[] args) {
-        PersonagemGame personagem1 = new PersonagemGame();
-        PersonagemGame personagem2 = new PersonagemGame();
-
-        personagem1.nome = "Sonic";
-        personagem1.saudeAtual = 10;
-
-        personagem2.nome = "Mario";
-        personagem2.saudeAtual = 100;
-
-        System.out.printf("Nome: %s - Saude: %d\n", personagem1.nome, personagem1.saudeAtual);
-        System.out.printf("Nome: %s - Saude: %d\n", personagem2.nome, personagem2.saudeAtual);
+   
+    public static void main(String[] var0) {
+       PersonagemGame var1 = new PersonagemGame();
+       PersonagemGame var2 = new PersonagemGame();
+       var1.nome = "Sonic";
+       var1.saudeAtual = 10;
+       var2.nome = "Mario";
+       var2.saudeAtual = 100;
+       System.out.printf("Nome: %s - Saude: %d\n", var1.nome, var1.saudeAtual);
+       System.out.printf("Nome: %s - Saude: %d\n", var2.nome, var2.saudeAtual);
     }
-}
+ }
+ 
