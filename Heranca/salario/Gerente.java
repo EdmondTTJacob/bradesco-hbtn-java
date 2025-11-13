d26e127abdc9b20aca9cfc97638551dd7f670169
EdmondTTJacob <edmondjacob.amd@gmail.com>
2025-11-13 14:43:10 +0000

    refact: heranca/salario

---
+++ Heranca/salario/Gerente.java
@@ -0,0 +1,18 @@
+package Heranca.salario;
+public class Gerente extends Empregado {
+
+    public Gerente(double salarioFixo) {
+        super(salarioFixo);
+    }
+
+    @Override
+    public double calcularBonus(Departamento departamento) {
+        if (departamento.alcancouMeta()) {
+            double bonus = this.getSalarioFixo() * 0.20;
+            double diferenca = departamento.getValorAtingidoMeta() - departamento.getValorMeta();
+            bonus += diferenca * 0.01;
+            return bonus;
+        }
+        return 0;
+    }
+}
