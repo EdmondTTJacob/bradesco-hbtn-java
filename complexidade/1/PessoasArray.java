7c0d4b980a99d202d44634aefd1a248d6828742d
EdmondTTJacob <edmondjacob.amd@gmail.com>
2025-11-21 17:07:10 +0000

    refactor: buscalinear

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -18,18 +18,14 @@ public class PessoasArray {
     }
 
     public void buscaLinear(String nome){
-        boolean encontrado = false;
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i].equals(nome)) {
                 System.out.println("Nome pesquisado é " + nomes[i] + " que está na posição " + i);
-                encontrado = true;
-                break;
+                return;
             }
         }
-        if (!encontrado) {
-            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
-        }
+        throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
     }
 }
