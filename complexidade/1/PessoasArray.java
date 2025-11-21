8b1d0a6743af3630520434bc1f943f0fbdd9fc92
EdmondTTJacob <edmondjacob.amd@gmail.com>
2025-11-21 17:28:59 +0000

    :fix: tentando ajeitar bug de validacao

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -19,13 +19,17 @@ public class PessoasArray {
 
     public void buscaLinear(String nome){
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
+        int posicao = -1;
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
-            if (nomes[i].equals(nome)) {
-                System.out.println("Nome pesquisado é " + nomes[i] + " que está na posição " + i);
-                return;
+            if (nomes[i].equals(nome) && posicao == -1) {
+                posicao = i;
             }
         }
-        throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        if (posicao != -1) {
+            System.out.println("Nome pesquisado é " + nome + " que está na posição " + posicao);
+        } else {
+            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
     }
 }
